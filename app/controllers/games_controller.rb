5bb46498c3d2e62ece87fa1765c22dd690eb9ffe
Joe Smith <joeycrushed@gmail.com>
2021-05-24 16:24:27 +0100

    attempt at longest word game

---
+++ app/controllers/games_controller.rb
@@ -0,0 +1,16 @@
+class GamesController < ApplicationController
+  def new
+    @letters = ('a'..'z').to_a.sample(10)
+  end
+
+  def score
+    @word = params[:word] # this is the word submitted
+    @english_word = english_word?(@word)
+  end
+
+  def english_word?(word)
+    response = URI.open("https://wagon-dictionary.herokuapp.com/#{word}")
+      json = JSON.parse(response.read)
+      return json['found']
+  end
+end
